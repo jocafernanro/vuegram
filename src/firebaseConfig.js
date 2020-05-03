dc4c9f8be4f83e07c055e0689bd0efc616030479
Jose Carlos Fernandez Rodriguez <jocafernanro@gmail.com>
2020-05-03 16:27:19 +0200

    ENV VARS

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,14 @@ import 'firebase/firestore'
 
 // firebase init goes here
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: ''
+  apiKey: `${process.env.VUE_APP_API_KEY}`,
+  authDomain: `${process.env.VUE_APP_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.VUE_APP_DATABASE_URL}`,
+  projectId: `${process.env.VUE_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.VUE_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.VUE_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.VUE_APP_APP_ID}`,
+  measurementId: `${process.env.VUE_APP_MEASUREMENT_ID}`
 }
 firebase.initializeApp(config)
 
@@ -18,9 +20,8 @@ const auth = firebase.auth()
 const currentUser = auth.currentUser
 
 // date issue fix according to firebase
-const settings = {
-  timestampsInSnapshots: true
-}
+const settings = {}
+
 db.settings(settings)
 
 // firebase collections
