9ed6d25bfaa8bc2989610ae9faafbbd13b8619ca
Jose Carlos Fernandez Rodriguez <jocafernanro@gmail.com>
2020-05-03 16:13:54 +0200

    Project working with firebase

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <div id="app">
+      <Navigation v-if="currentUser"></Navigation>
     <router-view/>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+import Navigation from './components/Navigation'
+
+export default {
+  components: { Navigation },
+  computed: {
+    ...mapState(['currentUser'])
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/scss/app.scss'
+
+const fb = require('./firebaseConfig.js')
 
 Vue.config.productionTip = false
 
@@ -10,3 +13,15 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+// handle page reloads
+let app
+fb.auth.onAuthStateChanged(user => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
